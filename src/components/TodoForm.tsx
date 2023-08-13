60d73760989fb34438434b0c1380fc949a074db0
Natthakornn <yhai0079@gmail.com>
2023-08-13 22:00:05 +0700

    change to text

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -15,7 +15,7 @@ export const TodoForm:React.FC<props> = ({addTodo}) => {
   }
   return (
     <form onSubmit={handleSubmit}>
-      <input type="test" className="placeholder-gray-500 border py-2 px-2 rounded" 
+      <input type="text" className="placeholder-gray-500 border py-2 px-2 rounded" 
       placeholder="Add Task Today?" onChange={(e) => setValue(e.target.value)}/>
       <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded">Add Task</button>
     </form>
